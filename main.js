63adb3a5023fe608629ef45a9b9d1a3a95ce9047
eunicorn2010 <105684586+eunicorn2010@users.noreply.github.com>
2022-07-20 16:07:17 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+
+
+function setup(){
+    canvas = createCanvas(600, 450);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
+}
+
+function draw() {
+    image
+    (video, 0, 0, 600, 500);
+}
+
